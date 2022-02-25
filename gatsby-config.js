2920eec7e06b1e6ade435bb4a2a25926f1a35d19
John Koestner <johnkoestner@outlook.com>
2022-02-24 23:19:43 -0500

    updating personal info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'John Koestner',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'John Koestner is a hobbyist and techie.',
+    siteUrl: 'https://jkoestner.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@jkstreamin',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'JohnKoestner',
+        short_name: 'JohnKoestner',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Actuarial', 'Python', 'SAS', 'SQL', 'Git'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +136,11 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I am passionate about technology, innovation, and creativity. Actuarial has
+              been my career where I am able to apply these ambitions. I have also enjoyed
+              learning about the space through some hobby projects. I hope to learn more
+              about how machine learning and predictive analytics will be able to continue
+              to see practical applications.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>

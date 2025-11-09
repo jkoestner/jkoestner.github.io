55fc12e4a54486bfa201f287cde5bd15bbbbe513
John Koestner <johnkoestner@outlook.com>
2025-11-08 23:29:39 -0500

    small update to image

--- src/components/loader.js
+++ src/components/loader.js
@@ -14,7 +14,7 @@ const StyledLoader = styled.div`
   width: 100%;
   height: 100%;
   background-color: var(--dark-navy);
-  background-image: url('/images/background/blueridge.png');
+  background-image: url('/images/background/blueridge.jpg');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
